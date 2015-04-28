98ae24ed13a6c09b530a40b01273bf683f3b199c
pamepeixinho <pameiupip@gmail.com>
2015-04-28 19:40:04 -0300

    gitignore

--- experimentos/EXECUTA_DiscrY.bat
+++
@@ -1,8 +0,0 @@
-echo off 
-cls 
-pause 
-
-cd\Users\pamela\Dropbox\IC-Pamela_(1)\Projeto\PhD-thesis_build\bin\Debug
-start SetupPadrao.exe imagens4\imagem 4 3 3 4 VERTICES_4 GRAFO_4 OCORRENCIA_4
-
-pause
--- experimentos/EXECUTA_Dist.bat
+++
@@ -1,8 +0,0 @@
-echo off 
-cls 
-pause 
-
-cd\Users\pamela\Dropbox\IC-Pamela_(1)\Projeto\PhD-thesis_build\bin\Debug
-start SetupPadrao.exe imagens4\imagem 4 3 3 1 VERTICES GRAFO OCORRENCIA
-
-pause
