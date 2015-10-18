74f4bb768263f94ce39c758b5a99e0bc08cfe3ef
pamepeixinho <pameiupip@gmail.com>
2015-10-18 20:12:12 -0200

    print saida

---
+++ TESTE_SetupPadrao_Orientacao.bat
@@ -0,0 +1,11 @@
+echo off 
+
+cls 
+
+pause 
+
+start 
+
+SetupPadrao.exe Orientacao C:/Users/pamela/Documents/IC/Study/project1/SUN2012_COMPLETA/SUN2012_TESTE/ 64 VerticeOrientacao GrafoOrientacao OcorrenciaOrientacao
+
+pause
